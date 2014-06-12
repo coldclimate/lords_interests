79f1c703fdd43cb8478ee8736bf4f9234159034c
coldclimate <oli@canddi.com>
2014-06-12 20:03:31 +0100

    do it for all files and remove empty pods

--- make.py
+++ make.py
@@ -1,36 +1,39 @@
 #!/usr/local/bin/python
 
 from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(open("data/A.html"))
 results = []
-top_level = soup.find_all("table", class_="lordsinteresttable")
 
-for lord in top_level:
-	lord_result={}
-	lord_result['name'] = lord.find_all("td", class_="lordsinterestmember")[0].string
-	lord_result['pods']=[]
-	pod = {}
-	for row in lord.find_all("td"):
-		try:
-			the_class = row['class'][0]
+for letter in ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]:
 
-		except KeyError:
-			the_class = "Notset"
-
-		if the_class == "lordsinterestmember":
-		    print ''
-		elif the_class == "lordsinterestcategory":
-		    print ''
-		    # file the existing one
-		    lord_result['pods'].append(pod)
-		    pod = {'type' : row.string, 'entries':[]}
-		else:
-		    pod['entries'].append(row.string)
-
-	results.append(lord_result)
-
-import json
+	soup = BeautifulSoup(open("data/" +letter +".html"))
+	top_level = soup.find_all("table", class_="lordsinteresttable")
+	
+	for lord in top_level:
+		lord_result={}
+		lord_result['name'] = lord.find_all("td", class_="lordsinterestmember")[0].string
+		lord_result['interests']=[]
+		pod = {}
+		for row in lord.find_all("td"):
+			try:
+				the_class = row['class'][0]
+	
+			except KeyError:
+				the_class = "Notset"
+	
+			if the_class == "lordsinterestmember":
+			    print ''
+			elif the_class == "lordsinterestcategory":
+			    print ''
+			    # file the existing one
+			    if pod != {}:
+			    	lord_result['interests'].append(pod)
+			    pod = {'type' : row.string, 'entries':[]}
+			else:
+			    pod['entries'].append(row.string)
+	
+		results.append(lord_result)
+	
+	import json
 print json.dumps(results,sort_keys=True, indent=4)
 
 
