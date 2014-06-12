c1fcb1b708883a2f84d13eb5e5060828057d2f4e
coldclimate <oli@canddi.com>
2014-06-12 19:56:18 +0100

    working for one file

--- make.py
+++ make.py
@@ -9,7 +9,29 @@ top_level = soup.find_all("table", class_="lordsinteresttable")
 for lord in top_level:
 	lord_result={}
 	lord_result['name'] = lord.find_all("td", class_="lordsinterestmember")[0].string
-	lord_result['name'] = lord.find_all("td", class_="lordsinterestmember")[0].string
+	lord_result['pods']=[]
+	pod = {}
+	for row in lord.find_all("td"):
+		try:
+			the_class = row['class'][0]
+
+		except KeyError:
+			the_class = "Notset"
+
+		if the_class == "lordsinterestmember":
+		    print ''
+		elif the_class == "lordsinterestcategory":
+		    print ''
+		    # file the existing one
+		    lord_result['pods'].append(pod)
+		    pod = {'type' : row.string, 'entries':[]}
+		else:
+		    pod['entries'].append(row.string)
+
 	results.append(lord_result)
 
-print results
+import json
+print json.dumps(results,sort_keys=True, indent=4)
+
+
+#print results
